bb53f284e98674ef6aeaef8db3242eb06b1ee3da
pierreh37@gmail.com <pierreh37@gmail.com@4b7bb204-8812-6a54-df07-7dd7ff4f54ac>
2013-04-01 19:20:39 +0000

    ajout engines

---
+++ otto/trunk/src/main/java/otto/process/engines/EngineShiFuMi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package otto.process.engines;
+
+import otto.process.dto.EngineRequest;
+import otto.process.dto.EngineResult;
+
+/**
+ *
+ * @author PIERRE
+ */
+public class EngineShiFuMi implements Engine{
+
+    @Override
+    public EngineResult process(EngineRequest req) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
