832a5cd12f7efc0bf1575c0e50ef856194eb2c6c
pierreh37 <pierreh37@gmail.com>
2013-04-07 15:48:14 +0000

    ajout IA et Jeu TicTacToe

--- otto/trunk/src/main/java/otto/process/engines/EngineTicTacToe.java
+++ otto/trunk/src/main/java/otto/process/engines/EngineTicTacToe.java
@@ -17,7 +17,13 @@ public class EngineTicTacToe implements Engine{
 
     @Override
     public EngineResultDTO process(EngineRequestDTO req) {
-        TicTacToeGame.Player iaplayer = TicTacToeGame.Player.X;
+        //convention joueur 1 = X joueur 2 = O
+        TicTacToeGame.Player iaplayer;
+        if(req.getTurn()%2==0){//pair
+            iaplayer = TicTacToeGame.Player.O;
+        }else{//impaire
+            iaplayer = TicTacToeGame.Player.X;
+        }
         TicTacToeGame ticTacToeGame = new TicTacToeGame(iaplayer, req.getTray());
         Move coup = (Move) new MinMaxAlphaBeta().calcIA(ticTacToeGame,iaplayer, 100);
         return new EngineResultDTO(Character.toString(coup.toBoardNumber()));
